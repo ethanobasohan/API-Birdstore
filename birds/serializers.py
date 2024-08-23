be14c776d4985715c8c77808cb68b4760600643b
ethanobasohan <83516411+ethanobasohan@users.noreply.github.com>
2024-08-22 22:54:01 -0400

    Create serializers.py

---
+++ birds/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Bird
+
+
+class BirdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bird
+        fields = ['name', 'price']
+
+
